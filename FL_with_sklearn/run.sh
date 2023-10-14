071a00a56348a6b4df30d7f560935cf09daa1d51
junyenchang <junyenchang@gmail.com>
2023-10-14 23:56:19 +0800

    revise data preprocess

--- FL_with_sklearn/run.sh
+++ FL_with_sklearn/run.sh
@@ -1,15 +1,16 @@
 #!/bin/bash
 
 echo "Starting server"
-python3 ./server.py &
+python ./server.py &
 sleep 3  # Sleep for 3s to give the server enough time to start
 
 for i in `seq 0 5`; do
   echo "Starting client $i"
-  python3 ./client.py &
+  python ./client.py &
 done
 
 # This will allow you to use CTRL+C to stop all background processes
 trap "trap - SIGTERM && kill -- -$$" SIGINT SIGTERM
 # Wait for all background processes to complete
 wait
+read -p "Press [Enter] to exit..."
